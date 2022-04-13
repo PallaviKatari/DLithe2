a9145ef3d25fdb3f527196b284a6cbd943887b9e
PallaviKatari <pallavikatari@gmail.com>
2022-04-13 15:04:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; //Basic Namespace-Datatypes,Arrays,Strings,I/O etc
+
+//create a namespace
+//Collection of classes
+namespace DLithe2
+{
+    //Summary   
+
+    /// <summary>
+    /// Introduction to C# Datatypes
+    /// </summary>
+    public class DLithe_Batch2
+    {
+        //Functions->Methods
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        public static void Main()
+        {
+            //System<-class.method
+            Console.WriteLine("Welcome to C# Programming");
+            
+
+        }
+    }
+
+
+}
